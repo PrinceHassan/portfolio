7faaa784e919f9eb6f7bcc6fbb2db727e8823d09
Hassan <ph0150163@gmail.com>
2023-02-12 20:34:00 +0500

    Chatbot created sucessfully! there is room for improvements

--- djangoProject1/urls.py
+++ djangoProject1/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from portfolio import views
 
 urlpatterns = [
     path('', include("portfolio.urls")),
+    path("reply_bot/", views.reply_bot),
 ]
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,12 +1,30 @@
 from django.shortcuts import render, HttpResponse
+# from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
+import openai
 
 # Create your views here.
 def index(request):
     return render(request, "index.html")
 
+
+# @csrf_exempt
 def reply_bot(request):
-    message = request.POST.get("inputData")
+    message = request.POST.get("input_data")
+    openai.api_key = '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'
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=message,
+        temperature=0.9,
+        max_tokens=50,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0.6,
+    )
+    response = response['choices'][0]['text']
+    user_message = f'<br clear="both"> <div class="item right"> <div class="msg"> <p>{message}</p> </div> </div>'
+    response_data = f'<div class="item"><div class="icon"><i class="fa fa-user"></i></div><div class="msg"><p>{response}</p></div></div>'
+    return JsonResponse({"userm": user_message,
+                         "resp": response_data,
+                         })
 
-
-
-    return HttpResponse(response_data)
